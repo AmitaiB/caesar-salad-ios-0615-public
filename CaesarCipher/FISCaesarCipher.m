c77f3a62534b00bbacc8fe899e2b20b381dad446
AmitaiB <blickstein@gmail.com>
2015-06-11 17:24:19 -0400

    passes all tests, clean and lean code, very happy

--- CaesarCipher/FISCaesarCipher.m
+++ CaesarCipher/FISCaesarCipher.m
@@ -10,30 +10,34 @@
 #import <objc/objc-runtime.h>
 
 @implementation FISCaesarCipher
-//define methods here
+
+//    decoding is encoding-backwards:
+-(NSString *)decodeWithMessage:(NSString *)encodedMessage andOffset:(NSInteger)key {
+    return [self encodeWithMessage:encodedMessage andOffset:(-1* key)];
+}
+
+//encoding is ecnoding:
 -(NSString*)encodeWithMessage:(NSString*)message andOffset:(NSInteger)key {
     NSCharacterSet *charsToEncode = [NSCharacterSet letterCharacterSet];
     NSCharacterSet *upperCaseLetters = [NSCharacterSet uppercaseLetterCharacterSet];
     NSCharacterSet *lowerCaseLetters = [NSCharacterSet lowercaseLetterCharacterSet];
-NSLog(@"Line 17: key = %ld", key);
+
+   
     //All multiples of 26 come "full-circle" around the alphabet. We get rid of them to simplify the problem.
     NSInteger modKey = key % 26;
-NSLog(@"Line 20: modKey = %ld", modKey);
     
-//    NSMutableString *workingMessage = [NSMutableString stringWithString:message];
+
     NSMutableArray *workingMessage = [[NSMutableArray alloc] init];
     for (NSInteger i = 0; i < message.length; i++) {
         NSInteger workingASCIIChar = [message characterAtIndex:i];
 
-        //        BOOL isLowercase = NO;
-NSLog(@"Loop %ul: workingASCIIChar = %C", i, workingASCIIChar);
         if ([charsToEncode characterIsMember: workingASCIIChar]) {
             BOOL isUppercase = [upperCaseLetters characterIsMember: workingASCIIChar] ? YES : NO;
             BOOL isLowercase = !isUppercase;
 
             workingASCIIChar += modKey; //the actual encoding
             
-    //        word-wrapping (z + 3 --> c)
+    //        word-wrapping (e.g., z + 3 --> c)
             if ((isUppercase && workingASCIIChar > 90) ||
                 (isLowercase && workingASCIIChar > 122))
                     workingASCIIChar -= 26;
@@ -51,11 +55,6 @@ NSLog(@"Loop %ul: workingASCIIChar = %C", i, workingASCIIChar);
     return [workingMessage componentsJoinedByString:@""];
 }
 
-
-
--(NSString *)decodeWithMessage:(NSString *)encodedMessage andOffset:(NSInteger)key {
-    return [self encodeWithMessage:encodedMessage andOffset:(-1* key)];
-}
 
 
 @end
