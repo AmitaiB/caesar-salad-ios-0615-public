14eb77d14402110e6f22d5dff4610c53fe46ba9d
AmitaiB <blickstein@gmail.com>
2015-06-11 15:10:23 -0400

    constructs for-loop with NSCharacterSets, unfinished

--- CaesarCipher/FISCaesarCipher.m
+++ CaesarCipher/FISCaesarCipher.m
@@ -10,6 +10,37 @@
 
 @implementation FISCaesarCipher
 //define methods here
+-(NSString*)encodeWithMessage:(NSString*)message andOffset:(NSInteger)key {
+    NSCharacterSet *charsToIgnore = [NSCharacterSet letterCharacterSet];
+    NSCharacterSet *upperCaseLetters = [NSCharacterSet uppercaseLetterCharacterSet];
+    NSCharacterSet *lowerCaseLetters = [NSCharacterSet lowercaseLetterCharacterSet];
+    
+    
+    NSMutableString *workingMessage = [NSMutableString stringWithString:message];
+    for (NSInteger i = 0; i < workingMessage.length; i++) {
+        NSInteger workingASCIIChar = [workingMessage characterAtIndex:i];
+        BOOL isUppercase = NO;
+//        BOOL isLowercase = NO;
+        
+        if ([[charsToIgnore characterIsMember: workingASCIIChar])
+             continue;
+        else if ([[charsToIgnore characterIsMember: workingASCIIChar])
+             
+             workingASCIIChar += 3;
+    
+             
+        
+        
+    }
+    
+    return @"";
+}
+
+
+
+-(NSString *)decodeWithMessage:(NSString *)encodedMessage andOffset:(NSInteger)key {
+    return [self encodeWithMessage:encodedMessage andOffset:(-1* key)];
+}
 
 
 @end
